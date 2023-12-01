f0f061027cd65be08f59f0419f79bf6fcbe262a9
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-12-02 02:20:58 +0800

    Update person_usecases.go

--- domain/usecases/person_usecases.go
+++ domain/usecases/person_usecases.go
@@ -9,4 +9,5 @@ type Person_Usecase interface {
 	NewPerson(context.Context, entity.Person) int
 	Login(context.Context, entity.Login_req) int
 	Logout(context.Context, entity.Login_req) int
+	GetFriends(context.Context, entity.Login_req) []entity.Person
 }
