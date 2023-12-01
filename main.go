f68a2977e1d3769ddce13bc00d7ea169c9d85f04
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-12-02 02:07:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"zhifubao/domain/repository"
+	"zhifubao/domain/usecases"
 	"zhifubao/person"
 	"zhifubao/transaction"
 
@@ -18,17 +20,17 @@ func main() {
 	client, _ := mongo.Connect(mainCtx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	database := client.Database("zhifubao")
 
-	var personData person.Database = person.NewRealMongo(database)
-	var fakeData person.Database = person.InitFake()
-	var personUC person.Usecase = person.NewUsecase(personData)
+	var personData repository.Person_Database = person.NewRealMongo(database)
+	//var fakeData repository.Person_Database = person.InitFake()
+	var personUC usecases.Person_Usecase = person.NewUsecase(personData)
 	var personHandler person.Handler = person.NewHandler(personUC, router)
 
-	var trcData transaction.Database = transaction.NewRealMongo(database)
-	var trcUC transaction.Usecase = transaction.NewUsecase(&personData, &trcData)
-	var trcHanlder transaction.Handler = transaction.NewHandler(&trcUC, router)
+	var trcData repository.Transaction_Database = transaction.NewRealMongo(database)
+	var trcUC usecases.Transaction_Usecase = transaction.NewUsecase(personData, trcData)
+	var trcHanlder transaction.Handler = transaction.NewHandler(trcUC, router)
 
 	personHandler.Standby(mainCtx)
 	trcHanlder.Standby(mainCtx)
 
-	router.Run("7480")
+	router.Run(":7480")
 }
