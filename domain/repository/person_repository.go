fa1d3b414d01f138623bde4dfceffbab0e5fae5b
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-12-02 02:09:54 +0800

    Create person_repository.go

---
+++ domain/repository/person_repository.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"context"
+	"zhifubao/domain/entity"
+)
+
+type Person_Database interface {
+	NewPerson(context.Context, entity.Person) int
+	GetPerson(context.Context, entity.Login_req) entity.Person
+	Login(context.Context, entity.Login_req) int
+	Logout(context.Context, entity.Login_req) int
+	IsActive(context.Context, entity.Login_req) bool
+	IsExist(context.Context, entity.Person) bool
+	ErrTest(context.Context) error
+	Autentication(context.Context, entity.Trc_req_one) (bool, error)
+	UpdateBalance(context.Context, entity.Transaction) bool
+}
