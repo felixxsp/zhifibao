b84c010701cdde77af06aa6a597b9d8186febcce
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-12-02 02:10:45 +0800

    Create person_usecases.go

---
+++ domain/usecases/person_usecases.go
@@ -0,0 +1,12 @@
+package usecases
+
+import (
+	"context"
+	"zhifubao/domain/entity"
+)
+
+type Person_Usecase interface {
+	NewPerson(context.Context, entity.Person) int
+	Login(context.Context, entity.Login_req) int
+	Logout(context.Context, entity.Login_req) int
+}
